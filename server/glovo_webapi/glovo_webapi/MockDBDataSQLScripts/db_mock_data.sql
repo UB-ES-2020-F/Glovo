9f1ee7cfd4981482a0b5d9362cb18a8ba9f1622c
RedReservoir <gerard.ortega88@gmail.com>
2020-10-28 23:55:53 +0100

    Task 6 implemented with functional and passing tests

---
+++ server/glovo_webapi/glovo_webapi/MockDBDataSQLScripts/db_mock_data.sql
@@ -0,0 +1,8 @@
+DELETE FROM "Restaurants";
+
+INSERT INTO "Restaurants" ("Id", "Name", "ImgPath")
+VALUES
+	(1, 'Kentucky Fried Chicken', 'img/KFC_main.jpg'),
+	(2, 'McDonalds', 'img/MD_main.jpg'),
+	(3, 'Burger King', 'img/BK_main.jpg'),
+	(4, 'Krusty Krab', 'img/KK_main.jpg')
