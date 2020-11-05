6f4c7b512552010a7db5325699788c09b5f1cd8c
mariomoliner <mario.moliner@gmail.com>
2020-11-05 10:33:08 +0100

    task for testing

--- frontend/customerapp/lib/screens/anon_bar.dart
+++ frontend/customerapp/lib/screens/anon_bar.dart
@@ -46,20 +46,44 @@ class AnonBar extends StatelessWidget with PreferredSizeWidget {
   }
 }
 
-class SearchBox extends StatelessWidget {
+class SearchBox extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() {
+    return SearchBox_Stateful();
+  }
+}
+
+class SearchBox_Stateful extends State<StatefulWidget> {
+  var isHoveredColor = Colors.white;
+
   @override
   Widget build(BuildContext context) {
     return InkWell(
         onTap: () {},
+        onHover: (value) {
+          if (value) {
+            setState(() {
+              isHoveredColor = hoverIconColor;
+            });
+          } else {
+            setState(() {
+              isHoveredColor = Colors.white;
+            });
+          }
+        },
+        hoverColor: Colors.transparent,
         child: Row(mainAxisSize: MainAxisSize.min, children: [
           IconButton(
             iconSize: 50.0,
-            color: Colors.white,
+            color: isHoveredColor,
+            hoverColor: Colors.transparent,
             icon: Icon(Icons.search_rounded),
             onPressed: () {},
           ),
-          TextLink('What do you need?', (context) {}, searchBarTextLinks,
-              searchBarTextLinksHover, context)
+          Text(
+            "What do you need?",
+            style: TextStyle(color: isHoveredColor, fontSize: 14),
+          )
         ]));
   }
 }
