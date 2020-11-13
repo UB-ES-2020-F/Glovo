bc18fbaa583a60319eff64d602ec0756a3f01372
eudald <eudald.elias@gmail.com>
2020-11-13 20:05:42 +0100

    deploy test

--- backend_deployment/deploy.sh
+++ backend_deployment/deploy.sh
@@ -2,6 +2,4 @@
 cd $TRAVIS_BUILD_DIR/backend_deployment
 dotnet ef migrations script --idempotent --project $TRAVIS_BUILD_DIR/server/glovo_webapi/glovo_webapi -o migration.sql
 heroku pg:psql --app ub-es2020-glovo-webapi < migration.sql
-git add .
-git commit -m "deploy"
-git push heroku master
+pwd
