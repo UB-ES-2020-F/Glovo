678d0178e84d3ddeae1902f4b0dfa771722a7116
eudald <eudald.elias@gmail.com>
2020-11-14 22:04:51 +0100

    test

--- backend_deployment/deploy.sh
+++ backend_deployment/deploy.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
+cd $TRAVIS_BUILD_DIR/server/glovo_webapi
+dotnet restore
+cd glovo_webapi
+dotnet publish -c Release -o $TRAVIS_BUILD_DIR/backend_deployment/publish
 cd $TRAVIS_BUILD_DIR/backend_deployment
 dotnet ef migrations script --idempotent --project $TRAVIS_BUILD_DIR/server/glovo_webapi/glovo_webapi -o migration.sql
 echo $HEROKU_API_KEY
