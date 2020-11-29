abf1b07c02aded4b4f830634eaa34936a49111c9
pedemonte96 <pedemonte96@users.noreply.github.com>
2020-11-30 00:06:15 +0100

    arrow on overview doesn't move

--- frontend/customerapp/lib/components/appBar/overview_logged_mobile.dart
+++ frontend/customerapp/lib/components/appBar/overview_logged_mobile.dart
@@ -17,48 +17,62 @@ class TabBar_screen extends StatelessWidget {
       Navigator.pop(context); //when resizing screen slowly, error occurs
     }
 
-    return DefaultTabController(
-      length: 2,
-      child: new Scaffold(
-        appBar: new PreferredSize(
-          preferredSize: Size.fromHeight(kToolbarHeight * 0.8 + 28),
-          child: new Container(
-            color: Colors.white,
-            child: new SafeArea(
-                child: Padding(
-              padding: EdgeInsets.only(right: 50, left: 50, top: 20),
-              child: Column(
-                children: <Widget>[
-                  new TabBar(
-                    labelStyle: NotSelectedStyle_bar,
-                    labelColor: Kommet_distinctive_yellow,
-                    unselectedLabelColor: Colors.black,
-                    indicatorPadding: EdgeInsets.only(top: 50),
-                    indicatorWeight: 4,
-                    onTap: (value) {},
-                    unselectedLabelStyle: SelectedStyle_bar,
-                    indicatorColor: Kommet_distinctive_yellow,
-                    tabs: [
-                      Padding(
-                          padding: EdgeInsets.only(bottom: 5),
-                          child: Text("Orders")),
-                      Padding(
-                          padding: EdgeInsets.only(bottom: 5),
-                          child: Text("Account"))
+    return Stack(
+      children: [
+        DefaultTabController(
+          length: 2,
+          child: new Scaffold(
+            appBar: new PreferredSize(
+              preferredSize: Size.fromHeight(kToolbarHeight * 0.8 + 28),
+              child: new Container(
+                color: Colors.white,
+                child: new SafeArea(
+                    child: Padding(
+                  padding: EdgeInsets.only(right: 50, left: 50, top: 20),
+                  child: Column(
+                    children: <Widget>[
+                      new TabBar(
+                        labelStyle: NotSelectedStyle_bar,
+                        labelColor: Kommet_distinctive_yellow,
+                        unselectedLabelColor: Colors.black,
+                        indicatorPadding: EdgeInsets.only(top: 50),
+                        indicatorWeight: 4,
+                        onTap: (value) {},
+                        unselectedLabelStyle: SelectedStyle_bar,
+                        indicatorColor: Kommet_distinctive_yellow,
+                        tabs: [
+                          Padding(
+                              padding: EdgeInsets.only(bottom: 5),
+                              child: Text("Orders")),
+                          Padding(
+                              padding: EdgeInsets.only(bottom: 5),
+                              child: Text("Account"))
+                        ],
+                      ),
                     ],
                   ),
-                ],
+                )),
               ),
-            )),
+            ),
+            body: new TabBarView(
+              children: <Widget>[
+                Subpage_orders(),
+                Subpage_user_profile(),
+              ],
+            ),
           ),
         ),
-        body: new TabBarView(
-          children: <Widget>[
-            Subpage_orders(),
-            Subpage_user_profile(),
-          ],
-        ),
-      ),
+        IconButton(
+          iconSize: 36,
+          icon: Image.asset(
+            'resources/images/arrow_diag.png',
+          ),
+          onPressed: () {
+            Navigator.pop(context);
+          },
+          alignment: Alignment(1, -1),
+        )
+      ],
     );
   }
 }
@@ -93,22 +107,6 @@ class Subpage_orders extends StatelessWidget {
                   ),
                 ],
               )),
-          Row(
-            children: [
-              Expanded(child: Container()),
-              IconButton(
-                iconSize: 40,
-                icon: Image.asset(
-                  'resources/images/arrow_diag.png',
-                  scale: 0.9,
-                ),
-                onPressed: () {
-                  Navigator.pop(context);
-                },
-                alignment: Alignment(1, -1),
-              )
-            ],
-          )
         ],
       ),
     );
@@ -131,23 +129,7 @@ class Subpage_user_profile extends StatelessWidget {
                   crossAxisAlignment: CrossAxisAlignment.start,
                   children: [
                     UserInformation(),
-                  ])),
-          Row(
-            children: [
-              Expanded(child: Container()),
-              IconButton(
-                iconSize: 40,
-                icon: Image.asset(
-                  'resources/images/arrow_diag.png',
-                  scale: 0.9,
-                ),
-                onPressed: () {
-                  Navigator.pop(context);
-                },
-                alignment: Alignment(1, -1),
-              )
-            ],
-          )
+                  ]))
         ]);
   }
 }
