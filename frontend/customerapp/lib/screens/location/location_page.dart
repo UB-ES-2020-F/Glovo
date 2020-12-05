e09b4d32e77aac40beea51728cc0a6feccafd61c
Ruben Ballester <rballeba@gmail.com>
2020-12-05 10:16:29 +0100

    Redirection for not logged users implemented in all necessary pages

--- frontend/customerapp/lib/screens/location/location_page.dart
+++ frontend/customerapp/lib/screens/location/location_page.dart
@@ -1,9 +1,15 @@
 import 'package:customerapp/screens/location/location_content.dart';
 import 'package:flutter/material.dart';
 
+import '../restricted_page.dart';
+
 class LocationPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    return RestrictedPage(this._build(context));
+  }
+
+  Widget _build(BuildContext context) {
     return Scaffold(
         body: Container(padding: EdgeInsets.all(30), child: LocationContent()));
   }
